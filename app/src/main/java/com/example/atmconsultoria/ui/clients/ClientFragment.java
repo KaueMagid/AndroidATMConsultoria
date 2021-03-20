4545e3bddce298807fd55b1d2bedbbfa3fb81b89
Kaue_Magid <magidsalem96@gmail.com>
2021-03-20 16:25:14 -0300

    Send to Notebook

---
+++ app/src/main/java/com/example/atmconsultoria/ui/clients/ClientFragment.java
@@ -0,0 +1,29 @@
+package com.example.atmconsultoria.ui.clients;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.atmconsultoria.R;
+
+
+public class ClientFragment extends Fragment {
+
+    public ClientFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_client, container, false);
+    }
+
+
+}
